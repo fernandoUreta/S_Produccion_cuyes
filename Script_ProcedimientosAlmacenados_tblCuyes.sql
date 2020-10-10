b073e22333a9ddd06159387204e806c83ceee373
Fernando Ureta <55604394+LestionY@users.noreply.github.com>
2020-10-09 22:00:22 -0500

    SP_Cuyes

---
+++ Script_ProcedimientosAlmacenados_tblCuyes.sql
@@ -0,0 +1,31 @@
+CREATE PROCEDURE SP_A_tblCuyes
+@id_Cuy as varchar(4),
+@id_Pozas as varchar(4),
+@id_Categoria as char(1),
+@genero as char(6),
+@fecha_naci as date
+as
+insert into tblCuyes values(@id_Cuy,@id_Pozas,@id_Categoria,@genero,@fecha_naci)
+
+EXEC SP_A_tblCuyes 'Cy1','B1','1','Macho','07/08/2020'
+
+CREATE PROCEDURE SP_C_tblCuyes
+@id_Cuy as varchar(4)
+as
+select *from tblCuyes
+
+CREATE PROCEDURE SP_M_tblCuyes
+@id_Cuy as varchar(4),
+@id_Pozas as varchar(4),
+@id_Categoria as char(1),
+@genero as char(6),
+@fecha_naci as date
+as
+UPDATE tblCuyes set ID_Pozas=@id_Pozas,ID_Categoria=@id_Categoria,cuyGenero=@genero,cuyFechaNaci=@fecha_naci
+
+CREATE PROCEDURE SP_E_tblCuyes
+@id_Cuy as varchar(4)
+as
+delete from tblCuyes where ID_Cuy=@id_Cuy
+
+
