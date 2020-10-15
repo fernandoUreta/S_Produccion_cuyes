2da2a5e1dacfe5e84d0335bf3edb5bb3100b60a2
Fernando Ureta <55604394+LestionY@users.noreply.github.com>
2020-10-15 18:31:18 -0500

    5

--- AppCuyes/app/src/main/java/com/example/appcuyes/MainActivity.java
+++ AppCuyes/app/src/main/java/com/example/appcuyes/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.appcuyes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner tiposPozas;
+    EditText txtAncho,txtLargo,txtCantidad,txtCantidadPozasCreadas;
+    Button btnRegistrar,btnMasInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registro_pozas);
+
     }
 }
