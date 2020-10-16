fdf59e6f09d332a52eb33cc2a62d208c957721b3
Fernando Ureta <55604394+LestionY@users.noreply.github.com>
2020-10-15 22:50:58 -0500

    clean

--- Version 6/ProyectoCuy/app/src/main/java/com/example/proyectocuy/ModeloDatos/Poza.java
+++
@@ -1,9 +0,0 @@
-package com.example.proyectocuy.ModeloDatos;
-
-public class Poza {
-    public String idPoza;
-    public float largo;
-    public float ancho;
-    public int capacidad;
-    public String clasificacion;
-}
