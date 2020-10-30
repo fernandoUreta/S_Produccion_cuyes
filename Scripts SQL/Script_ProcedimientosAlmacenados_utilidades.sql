d8b6106bb53ebd510871d48373d22547473021ca
Fernando Ureta <55604394+LestionY@users.noreply.github.com>
2020-10-30 13:03:21 -0500

    now Yes

--- Scripts SQL/Script_ProcedimientosAlmacenados_utilidades.sql
+++ Scripts SQL/Script_ProcedimientosAlmacenados_utilidades.sql
@@ -16,6 +16,7 @@ CREATE PROCEDURE SP_C_CuyesEdadMax
 @edadMaxima as int
 as
 declare @fechahoy DATE=(GETDATE())
-SELECT ID_Cuy,DATEDIFF (DAY, @fechahoy , cuyFechaNaci )AS Edad,ID_Pozas INTO #Lista FROM tblCuyes WHERE ID_Pozas in (SELECT ID_Pozas FROM tblPozas WHERE pozClasificacion='Recría');
-SELECT ID_Cuy,ID_Pozas, edad FROM #Lista WHERE Edad>@edadMaxima
+SELECT ID_Cuy,DATEDIFF (DAY,cuyFechaNaci,@fechahoy)AS Edad,ID_Pozas INTO #Lista FROM tblCuyes WHERE ID_Pozas in (SELECT ID_Pozas FROM tblPozas WHERE pozClasificacion=@tipoPoza);
+SELECT ID_Cuy,ID_Pozas, edad FROM #Lista WHERE Edad>=@edadMaxima
 DROP TABLE #Lista 
+
