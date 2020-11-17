baf02f14b1a44bdbad09b9528809ea49fe0a1586
Fernando Ureta <55604394+LestionY@users.noreply.github.com>
2020-11-16 19:59:46 -0500

    gitignore

--- ProyectoCompartido/app/src/main/java/com/example/appproduccioncuyes/ConexionBD/ConexionSQLServer.java
+++ ProyectoCompartido/app/src/main/java/com/example/appproduccioncuyes/ConexionBD/ConexionSQLServer.java
@@ -7,34 +7,20 @@ import java.sql.DriverManager;
 
 public class ConexionSQLServer {
 
-    public static Connection conectarBD(){
-        Connection cnn=null;
+    public static Connection conectarBD() {
+        Connection cnn = null;
         try {
             StrictMode.ThreadPolicy politica = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(politica);
 
             Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
-            cnn= DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.5.103;databaseName=BDProduccionCuyes;user=sa;password=123456;");
-        }catch (Exception e){
+            cnn = DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.0.103;databaseName=BDProduccionCuyes;user=sa;password=123456;");
+        } catch (Exception e) {
             return null;
         }
         return cnn;
     }
-
-    public static boolean ProbarConexion ()
-    {
-        Connection cnn=null;
-        try {
-            StrictMode.ThreadPolicy politica = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(politica);
-
-            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
-            cnn= DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.1.44;databaseName=BDProduccionCuyes;user=sa;password=123456;");
-            return true;
-        }catch (Exception e){
-            return false;
-        }
-    }
+}
 
 
 
