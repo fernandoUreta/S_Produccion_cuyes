bebc33ee28b54dc31e35988564181c3a236d75af
Fernando Ureta <55604394+LestionY@users.noreply.github.com>
2020-11-19 16:57:09 -0500

    Revert "Revert "Revert "hora si"""
    This reverts commit a8fef96bb7a2c99c51dcdfbbc4868075601e7e11.

--- Aplicacion_de_crianza_de_cuyes/ProyectoCuy/app/src/main/java/com/example/proyectocuy/MenuPozasEngorde.java
+++ Aplicacion_de_crianza_de_cuyes/ProyectoCuy/app/src/main/java/com/example/proyectocuy/MenuPozasEngorde.java
@@ -37,12 +37,12 @@ public class MenuPozasEngorde extends AppCompatActivity {
     private void cargarDatos() {
         ArrayList<String> listaA1=new ArrayList<>();
 
-        for (int i=1;i<=BD_ProduccionCuyes.consultarCantidadPozasEngorde();i++){
+        for (int i=1;i<=BD_ProduccionCuyes.consultarCantidadPozasEmpadre();i++){
             listCategoria.add("B"+i);
         }
 
         listaA1.add("1");
-        for (int i=0;i<BD_ProduccionCuyes.consultarCantidadPozasEngorde();i++){
+        for (int i=0;i<BD_ProduccionCuyes.consultarCantidadPozasEmpadre();i++){
             mapChild.put(listCategoria.get(i),listaA1);
         }
 
