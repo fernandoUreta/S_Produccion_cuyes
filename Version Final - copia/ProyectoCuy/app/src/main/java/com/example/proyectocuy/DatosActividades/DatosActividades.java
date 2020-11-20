150314f0f7742e3ccb1ef89dc8358aade96efd65
Fernando Ureta <55604394+LestionY@users.noreply.github.com>
2020-11-20 00:13:55 -0500

    delete no usados

--- Version Final - copia/ProyectoCuy/app/src/main/java/com/example/proyectocuy/DatosActividades/DatosActividades.java
+++
@@ -1,8 +0,0 @@
-package com.example.proyectocuy.DatosActividades;
-
-public class DatosActividades {
-    public String Engorde;
-    public String Padrillo;
-    public String Recria;
-    public String Empadre;
-}
--- Version Final/ProyectoCuy/app/src/main/java/com/example/proyectocuy/Usuarios.java
+++
@@ -1,6 +0,0 @@
-package com.example.proyectocuy;
-
-public class Usuarios {
-    String Correo;
-    String Contraseña;
-}
