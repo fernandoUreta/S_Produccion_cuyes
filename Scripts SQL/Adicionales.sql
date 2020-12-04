96ea11aa69c81b7e6c6807532fe4f8b04bdae073
Fernando Ureta <55604394+LestionY@users.noreply.github.com>
2020-12-04 16:34:59 -0500

    Update Adicionales.sql

--- Scripts SQL/Adicionales.sql
+++ Scripts SQL/Adicionales.sql
@@ -4,3 +4,11 @@ CREATE PROCEDURE SP_C_tblUsuario_byCorreo
 as
 SELECT * FROM tblUsuario where usuCorreo=@correo
 
+//determinar tipo poza
+CREATE PROCEDURE SP_validar_tipoPoza
+@idcategoria AS CHAR(2),
+@genero AS CHAR(6),
+@idPoza AS VARCHAR(4)
+AS
+select count(ID_Cuy) from tblCuyes INNER JOIN tblPozas on tblCuyes.ID_Pozas=tblPozas.ID_Pozas and
+tblCuyes.ID_Categoria=@idcategoria and cuyGenero=@genero and tblCuyes.ID_Pozas=@idPoza
