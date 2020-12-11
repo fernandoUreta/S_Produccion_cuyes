177994917c709e00ba1039d65b1c15fd51df4b0a
Fernando Ureta <55604394+LestionY@users.noreply.github.com>
2020-12-10 23:34:00 -0500

    clean

--- z_papelera de reciclaje/AppCuyes/app/src/main/java/com/example/appcuyes/ModeloDatos/Poza.java
+++
@@ -1,9 +0,0 @@
-package com.example.appcuyes.ModeloDatos;
-
-public class Poza {
-    public String idPoza;
-    public float largo;
-    public float ancho;
-    public int capacidad;
-    public String clasificacion;
-}
